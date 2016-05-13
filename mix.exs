30366077ff5f115c22401acbfa3dd9d7b3433b2e
Jon Gretar <jongretar@jongretar.com>
2016-05-13 00:22:32 +0000

    Publishing

--- mix.exs
+++ mix.exs
@@ -6,6 +6,11 @@ defmodule Kennitala.Mixfile do
       app: :kennitala,
       version: "0.0.1",
       elixir: "~> 1.1",
+      description: description,
+      package: package,
+      name: "Kennitala",
+      source_url: "https://github.com/JonGretar/Kennitala.ex",
+      homepage_url: "https://github.com/JonGretar/Kennitala.ex",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
@@ -19,6 +24,26 @@ defmodule Kennitala.Mixfile do
   end
 
   defp deps do
-    [{:excoveralls, "~> 0.5", only: :test}]
+    [
+      {:excoveralls, "~> 0.5", only: :test},
+      {:ex_doc, "~> 0.11", only: :docs},
+      {:ex_doc_dash, "~> 0.3", only: :docs}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir library for validating and handling the Icelandic Kennitala identity number.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Jón Grétar Borgþórsson"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub": "https://github.com/JonGretar/Kennitala.ex"
+      }
+    ]
   end
 end
